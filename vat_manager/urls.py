a12822cb9bd6594c7e7e877eff54ad509f3861b1
Diogo Silva <diogo_silva_555@hotmail.com>
2023-10-26 13:40:27 -0300

    Adicionado endpoint para retornar todas as cotacoes persistidas no banco

--- vat_manager/urls.py
+++ vat_manager/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from vat_manager.views import get_exchange_rate_data
+from vat_manager.views import get_exchange_rate_data, TableView
 
 name_app = 'vat_manager'
 
 
 urlpatterns = [
     path('get_exchange_rate_data', get_exchange_rate_data, name='consultar_cotacoes'),
+    path('table_view', TableView.as_view(), name='table_view')
 ]
 
 
