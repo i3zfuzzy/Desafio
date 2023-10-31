fba4ddf3bf05b832c59d280b65141f826f94bd79
Diogo Silva <diogo_silva_555@hotmail.com>
2023-10-30 23:52:42 -0300

    voltado para versao anterior

--- vat_manager/tests/tests.py
+++ vat_manager/tests/tests.py
@@ -1,45 +1,15 @@
-import unittest
-from datetime import datetime, timedelta
-from unittest.mock import MagicMock
-import asyncio
-
-from asgiref.sync import sync_to_async
-
-from vat_manager.views import get_datas_between_period, date_range, fetch_and_save_exchange_rates, \
-    get_exchange_rate_data, TableView
+from django.test import TestCase, Client
+from django.urls import reverse
 
 
-class TestYourFunctions(unittest.TestCase):
-
-    def test_get_datas_between_period(self):
-        start_date = "2023-10-20"
-        end_date = "2023-10-25"
-        expected_dates = [datetime(2023, 10, 20), datetime(2023, 10, 21), datetime(2023, 10, 22),
-                          datetime(2023, 10, 23), datetime(2023, 10, 24), datetime(2023, 10, 25)]
-        self.assertEqual(get_datas_between_period(start_date, end_date), expected_dates)
-
-    def test_date_range(self):
-        date_s = "2023-10-20"
-        date_e = "2023-10-25"
-        self.assertTrue(date_range(date_s, date_e))
-
-    @sync_to_async
-    def test_fetch_and_save_exchange_rates(self):
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(fetch_and_save_exchange_rates("2023-10-20", "2023-10-25"))
+class TestExchangeRateView(TestCase):
+    def setUp(self):
+        self.client = Client()
 
     def test_get_exchange_rate_data(self):
-        mock_request = MagicMock()
-        mock_request.GET.get.return_value = '2023-10-20'
-        # Add more mock attributes as needed
-
-        response = get_exchange_rate_data(mock_request)
+        response = self.client.get('/get_exchange_rate_data?start_date=2023-01-01&end_date=2023-01-05')
+        self.assertEqual(response.status_code, 200)
 
     def test_table_view(self):
-        mock_request = MagicMock()
-
-        response = TableView().get(mock_request)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        response = self.client.get(reverse('table_view'))
+        self.assertEqual(response.status_code, 200)
--- vat_manager/views.py
+++ vat_manager/views.py
@@ -1,10 +1,9 @@
+import requests
 from collections import defaultdict
 from datetime import datetime, timedelta
-import aiohttp
 from django.db.models import Q, Max
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
-from django.utils import asyncio
 from django.views import View
 
 from .models import ExchangeRate
@@ -40,39 +39,27 @@ def date_range(date_s, date_e):
 
 
 # Valida o range de data da request, salva o objeto no banco
-async def fetch_exchange_rate(session, date, base_url, currency):
-    async with session.get(f"{base_url}?date={date}&base=USD") as response:
-        data = await response.json()
-        return data["rates"][currency]
-
-
-async def fetch_and_save_exchange_rates(start_date, end_date):
+def fetch_and_save_exchange_rates(start_date, end_date):
     base_url = "https://api.vatcomply.com/rates"
     currencies = ["EUR", "JPY", "BRL"]
     dates_between_period = get_datas_between_period(start_date, end_date)
+    for date in dates_between_period:
+        if not ExchangeRate.objects.filter(date=date).exists():
+            for currency in currencies:
+                response = requests.get(f"{base_url}?date={start_date}&base=USD")
+                data = response.json()
+                ExchangeRate.objects.create(
+                    date=start_date,
+                    base_currency="USD",
+                    target_currency=currency,
+                    rate=data["rates"][currency]
+                )
+            break
 
-    async with aiohttp.ClientSession() as session:
-        tasks = []
-        for date in dates_between_period:
-            if not ExchangeRate.objects.filter(date=date).exists():
-                for currency in currencies:
-                    task = asyncio.create_task(fetch_exchange_rate(session, date, base_url, currency))
-                    tasks.append(task)
-
-        results = await asyncio.gather(*tasks)
-
-        for i, date in enumerate(dates_between_period):
-            if not ExchangeRate.objects.filter(date=date).exists():
-                for j, currency in enumerate(currencies):
-                    ExchangeRate.objects.create(
-                        date=date,
-                        base_currency="USD",
-                        target_currency=currency,
-                        rate=results[i * len(currencies) + j]
-                    )
+    else:
+        return HttpResponse("Data informada não corresponde a 5 dias úteis")
 
 
-# Recebe a request do usuário e monta a base de dados do gráfico
 def get_exchange_rate_data(request):
     start_date = request.GET.get('start_date')
     end_date = request.GET.get('end_date')
@@ -80,9 +67,6 @@ def get_exchange_rate_data(request):
         dates_between_period = get_datas_between_period(start_date, end_date)
         for date in dates_between_period:
             fetch_and_save_exchange_rates(date.strftime("%Y-%m-%d"), end_date)
-    else:
-        return HttpResponse("Data informada não correponde à 5 dias úteis")
-
     data = ExchangeRate.objects.filter(Q(date__gte=start_date), Q(date__lte=end_date))
     unique_dates = data.values('date').annotate(Max('date'))
     unique_data = [str(datum['date__max']) for datum in unique_dates]
@@ -100,7 +84,6 @@ def get_exchange_rate_data(request):
                   {'dates': unique_data, 'usd_to_eur': usd_to_eur, 'usd_to_brl': usd_to_brl, 'usd_to_jpy': usd_to_jpy})
 
 
-# Retorna todas as cotaçoes persistidas no banco
 class TableView(View):
     def get(self, request):
         data = list(ExchangeRate.objects.values())
